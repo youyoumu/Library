2fd5442c76c161693949a18c8f9aee6766ddcb7b
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-04-05 12:52:09 +0700

    Delete old code

--- script.js
+++ script.js
@@ -90,59 +90,6 @@ class Gui {
 const library = new Library()
 const gui = new Gui(library)
 
-// books = document.getElementById('books')
-
-// function addBookToLibrary() {
-//     title = form.elements.title.value
-//     author = form.elements.author.value
-//     pages = form.elements.pages.value
-//     read = form.elements.read.checked
-//     myLibrary.push(new Book(title, author, pages, read))
-//     form.elements.title.value = ''
-//     form.elements.author.value = ''
-//     form.elements.pages.value = ''
-//     form.elements.read.checked = false
-//     displayBooks()
-// }
-
-// function displayBooks() {
-//     books.innerHTML = ''
-//     for (let i = 0; i < myLibrary.length; i++) {
-//         if (myLibrary[i].read === true) {
-//             books.innerHTML += `<div class="card">
-//         <p>${myLibrary[i].info()}</p> 
-//         <button class="removeButton" data-attribute="${i}">Remove</button>
-//         <button class="readButton" data-attribute="${i}">Have Read</button>
-//         </div>`
-//         } else {
-//             books.innerHTML += `<div class="card">
-//         <p>${myLibrary[i].info()}</p> 
-//         <button class="removeButton" data-attribute="${i}">Remove</button>
-//         <button class="readButton" data-attribute="${i}">Haven't Read</button>
-//         </div>`
-//         }
-
-//     }
-//     removeButton = document.querySelectorAll('.removeButton')
-//     removeButton.forEach(button => button.addEventListener('click', (e) => {
-//         console.log(e.target.dataset.attribute)
-//         myLibrary.splice(e.target.dataset.attribute, 1)
-//         displayBooks()
-//     }))
-
-//     readButton = document.querySelectorAll('.readButton')
-//     readButton.forEach(button => button.addEventListener('click', (e) => {
-//         myLibrary[e.target.dataset.attribute].read = !myLibrary[e.target.dataset.attribute].read
-//         displayBooks()
-//     }))
-// }
-
-// form = document.getElementById('form')
-// form.addEventListener('submit', (e) => {
-//     e.preventDefault()
-//     addBookToLibrary()
-// })
-
 
 
 
